3972dd583b8f8556a2d17a786be9cab8b8eb065a
박범진 <qjawlsqjacks@naver.com>
2022-06-25 18:29:59 +0900

    외부반복 내부반복

---
+++ ModernJava/src/Dish.java
@@ -0,0 +1,76 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Dish {
+
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type {
+        MEAT,
+        FISH,
+        OTHER
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public static final List<Dish> menu = Arrays.asList(
+            new Dish("pork", false, 800, Dish.Type.MEAT),
+            new Dish("beef", false, 700, Dish.Type.MEAT),
+            new Dish("chicken", false, 400, Dish.Type.MEAT),
+            new Dish("french fries", true, 530, Dish.Type.OTHER),
+            new Dish("rice", true, 350, Dish.Type.OTHER),
+            new Dish("season fruit", true, 120, Dish.Type.OTHER),
+            new Dish("pizza", true, 550, Dish.Type.OTHER),
+            new Dish("prawns", false, 400, Dish.Type.FISH),
+            new Dish("salmon", false, 450, Dish.Type.FISH)
+    );
+
+    public List<String> internalIteration() {
+        return menu.stream()
+                .filter(d -> d.getCalories() > 300)
+                .map(Dish::getName)
+                .collect(Collectors.toList());
+    }
+
+    public static void forEach() {
+//        menu.stream().forEach(dish -> { System.out.println(dish.getName()); });
+        menu.forEach(dish -> System.out.println(dish.getName()));
+//        menu.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) throws IOException {
+        forEach();
+    }
+
+}

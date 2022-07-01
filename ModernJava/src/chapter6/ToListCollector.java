ebc34cf9193841e288f7e801f0982c7c95a652e3
박범진 <qjawlsqjacks@fasoo.com>
2022-07-01 13:18:35 +0900

    Collector

---
+++ ModernJava/src/chapter6/ToListCollector.java
@@ -0,0 +1,40 @@
+package chapter6;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class ToListCollector<T> implements Collector<T, List<T>, List<T>> {
+    @Override
+    public Supplier<List<T>> supplier() {
+        return () -> new ArrayList<T>();
+    }
+
+    @Override
+    public BiConsumer<List<T>, T> accumulator() {
+        return (list, item) -> list.add(item);
+    }
+
+    @Override
+    public BinaryOperator<List<T>> combiner() {
+        return (list1, list2) -> {
+            list1.addAll(list2);
+            return list1;
+        };
+    }
+
+    @Override
+    public Function<List<T>, List<T>> finisher() {
+        return Function.identity();
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return null;
+    }
+}

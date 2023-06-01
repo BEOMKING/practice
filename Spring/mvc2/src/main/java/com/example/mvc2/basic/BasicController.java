39f84a933923ad2114841269921eb79298822214
BEOMKING <qjawlsqjacks@naver.com>
2023-06-01 23:49:31 +0900

    Thymeleaf Practice

---
+++ Spring/mvc2/src/main/java/com/example/mvc2/basic/BasicController.java
@@ -0,0 +1,88 @@
+package com.example.mvc2.basic;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(final Model model) {
+        model.addAttribute("data", "Hello Spring!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/text-unescaped")
+    public String textUnescaped(final Model model) {
+        model.addAttribute("data", "Hello <b>Spring!</b>");
+        return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(final Model model) {
+        final User userA = new User("userA", 10);
+        final User userB = new User("userB", 20);
+
+        final List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        final Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(final Model model, final HttpServletRequest request,
+        final HttpServletRequest response, final HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+
+        return "basic/basic-objects";
+    }
+
+    static class User {
+
+        private final String username;
+        private final int age;
+
+        public User(final String username, final int age) {
+            this.username = username;
+            this.age = age;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(final String data) {
+            return "Hello " + data;
+        }
+    }
+}

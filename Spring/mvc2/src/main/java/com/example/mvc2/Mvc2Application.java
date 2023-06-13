935f9dc52b219f6e8581fe4ad26d5b192b1b14df
BEOMKING <qjawlsqjacks@naver.com>
2023-06-13 23:52:08 +0900

    Locale Intro

--- Spring/mvc2/src/main/java/com/example/mvc2/Mvc2Application.java
+++ Spring/mvc2/src/main/java/com/example/mvc2/Mvc2Application.java
@@ -2,6 +2,9 @@ package com.example.mvc2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class Mvc2Application {
@@ -10,4 +13,11 @@ public class Mvc2Application {
 		SpringApplication.run(Mvc2Application.class, args);
 	}
 
+//	@Bean
+//	public MessageSource messageSource() {
+//		final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("UTF-8");
+//		return messageSource;
+//	}
 }

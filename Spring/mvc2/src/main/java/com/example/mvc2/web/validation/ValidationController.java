a11e9efd88c9d14f639c91aa65724512a6fbd14b
BEOMKING <qjawlsqjacks@naver.com>
2023-06-16 15:13:06 +0900

    BindingResult 개선

--- Spring/mvc2/src/main/java/com/example/mvc2/web/validation/ValidationController.java
+++ Spring/mvc2/src/main/java/com/example/mvc2/web/validation/ValidationController.java
@@ -10,8 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -107,24 +105,56 @@ public class ValidationController {
 //        return "redirect:/validation/items/{itemId}";
 //    }
 
+//    @PostMapping("/add")
+//    public String addItemV2(@ModelAttribute Item item, final BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+//        if (!StringUtils.hasText(item.getItemName())) {
+//            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null));
+//        }
+//
+//        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+//            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+//        }
+//
+//        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+//            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+//        }
+//
+//        if (item.getPrice() != null && item.getQuantity() != null) {
+//            final int resultPrice = item.getPrice() * item.getQuantity();
+//            if (resultPrice < 10000) {
+//                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
+//            }
+//        }
+//
+//        if (bindingResult.hasErrors()) {
+//            log.error("errors={}", bindingResult);
+//            return "validation/addForm";
+//        }
+//
+//        final Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("itemId", savedItem.getId());
+//        redirectAttributes.addAttribute("status", true);
+//        return "redirect:/validation/items/{itemId}";
+//    }
+
     @PostMapping("/add")
-    public String addItemV2(@ModelAttribute Item item, final BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public String addItemV3(@ModelAttribute Item item, final BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+            bindingResult.rejectValue("itemName", "required");
         }
 
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
         }
 
         if (item.getQuantity() == null || item.getQuantity() > 9999) {
-            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
 
         if (item.getPrice() != null && item.getQuantity() != null) {
             final int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
-                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = " + resultPrice));
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
 

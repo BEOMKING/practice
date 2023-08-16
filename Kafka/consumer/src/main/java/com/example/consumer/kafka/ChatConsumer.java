ba37138420c0de1a5ce1c2ec5e728ec75a2a3406
BEOMKING <qjawlsqjacks@naver.com>
2023-08-17 00:29:27 +0900

    컨슈머 역직렬화 로직 추가 및 기록

--- Kafka/consumer/src/main/java/com/example/consumer/kafka/ChatConsumer.java
+++ Kafka/consumer/src/main/java/com/example/consumer/kafka/ChatConsumer.java
@@ -2,31 +2,24 @@ package com.example.consumer.kafka;
 
 import com.example.consumer.domain.Chat;
 import com.example.consumer.repository.ChatRepository;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @Slf4j
 @Service
 public class ChatConsumer {
     private final ChatRepository chatRepository;
-    private final ObjectMapper objectMapper;
 
-    public ChatConsumer(final ChatRepository chatRepository, final ObjectMapper objectMapper) {
+    public ChatConsumer(final ChatRepository chatRepository) {
         this.chatRepository = chatRepository;
-        this.objectMapper = objectMapper;
     }
 
-    @KafkaListener(topics = "chat")
-    public void recordChat(final String message) throws JsonProcessingException {
-        log.info("Consumed message : {}", message);
-        final Chat chat = objectMapper.readValue(message, Chat.class);
-        chatRepository.save(chat);
+    @KafkaListener(id = "chatListener1", topics = "chat")
+    public void recordChat(final List<Chat> chat) {
+        log.info("Consumed message : {}", chat.size());
+        chat.forEach(chatRepository::save);
     }
 }
--- Kafka/consumer/src/main/java/com/example/consumer/kafka/KafkaConsumerConfig.java
+++ Kafka/consumer/src/main/java/com/example/consumer/kafka/KafkaConsumerConfig.java
@@ -1,14 +1,15 @@
 package com.example.consumer.kafka;
 
+import com.example.consumer.domain.Chat;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.Map;
 
@@ -16,23 +17,21 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
     @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(producerConfigs());
+    public ConsumerFactory<String, Chat> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(producerConfigs(), new StringDeserializer(), new JsonDeserializer<>(Chat.class));
     }
 
     @Bean
     public Map<String, Object> producerConfigs() {
         return Map.of(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
-                ConsumerConfig.GROUP_ID_CONFIG, "chatGroupId",
-                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class
+                ConsumerConfig.GROUP_ID_CONFIG, "chatGroupId"
         );
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-        final ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    public ConcurrentKafkaListenerContainerFactory<String, Chat> kafkaListenerContainerFactory() {
+        final ConcurrentKafkaListenerContainerFactory<String, Chat> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         factory.isBatchListener();
         return factory;

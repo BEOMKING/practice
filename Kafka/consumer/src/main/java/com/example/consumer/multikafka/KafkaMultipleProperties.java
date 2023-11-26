b984fa039cd278bf67981929fc68b4b8e7b1849c
BEOMKING <qjawlsqjacks@naver.com>
2023-11-26 22:59:47 +0900

    KafkaMultipleConsumerConfig 구현

--- Kafka/consumer/src/main/java/com/example/consumer/multikafka/KafkaMultipleProperties.java
+++ Kafka/consumer/src/main/java/com/example/consumer/multikafka/KafkaMultipleProperties.java
@@ -1,10 +1,11 @@
 package com.example.consumer.multikafka;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,11 +13,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "kafka")
 public class KafkaMultipleProperties {
     private List<String> bootstrapServers = new ArrayList<>(Collections.singletonList("localhost:9092"));
-    private Map<String, String> properties = new HashMap<>();
     private Map<String, KafkaProperties.Producer> producers;
     private Map<String, KafkaProperties.Consumer> consumers;
     private KafkaProperties.Ssl ssl = new KafkaProperties.Ssl();
@@ -31,10 +33,6 @@ public class KafkaMultipleProperties {
 
         properties.putAll(this.ssl.buildProperties());
         properties.putAll(this.security.buildProperties());
-
-        if (!CollectionUtils.isEmpty(this.properties)) {
-            properties.putAll(this.properties);
-        }
 
         return properties;
     }

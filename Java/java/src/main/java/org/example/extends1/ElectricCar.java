dfad8af6c6fc910e0ad7e20f98165ca435744cb2
BEOMKING <qjawlsqjacks@naver.com>
2023-12-27 21:57:13 +0900

    전기, 가스 자동차 구현

---
+++ Java/java/src/main/java/org/example/extends1/ElectricCar.java
@@ -0,0 +1,11 @@
+package org.example.extends1;
+
+public class ElectricCar {
+    public void charge() {
+        System.out.println("Charging...");
+    }
+
+    public void move() {
+        System.out.println("Moving...");
+    }
+}
---
+++ Java/java/src/main/java/org/example/extends1/GasCar.java
@@ -0,0 +1,11 @@
+package org.example.extends1;
+
+public class GasCar {
+    public void fillUp() {
+        System.out.println("Filling up...");
+    }
+
+    public void move() {
+        System.out.println("Moving...");
+    }
+}

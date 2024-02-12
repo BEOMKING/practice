8b356e19e6ed163b0b1b9eb99aa8e0d7bdc0a2bc
BEOMKING <qjawlsqjacks@naver.com>
2024-02-12 19:06:51 +0900

    Push base setting.

---
+++ Spring/spring-db/src/main/java/org/example/springdb/SpringDbApplication.java
@@ -0,0 +1,13 @@
+package org.example.springdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbApplication.class, args);
+	}
+
+}

8e856ab3b3e6b4f4d115f940e4f1b36548151e20
BEOMKING <qjawlsqjacks@naver.com>
2024-02-18 22:26:45 +0900

    DriverManagerDataSource connection test

---
+++ Spring/spring-db/src/test/java/org/example/springdb/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,50 @@
+package org.example.springdb.jdbc.connection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.example.springdb.jdbc.connection.ConnectionConst.*;
+
+class ConnectionTest {
+    @Test
+    void getDriverManager() throws SQLException {
+        final Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        final Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+        System.out.println("connection1 = " + connection1);
+        System.out.println("connection1 = " + connection1.getClass());
+        System.out.println("connection2 = " + connection2);
+        System.out.println("connection2 = " + connection2.getClass());
+    }
+
+    @Test
+    void getDataSourceDriverManager() throws SQLException {
+        final DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        userDataSource(dataSource);
+    }
+
+    private void userDataSource(DataSource dataSource) throws SQLException {
+        final Connection connection1 = dataSource.getConnection();
+        final Connection connection2 = dataSource.getConnection();
+        System.out.println("connection1 = " + connection1);
+        System.out.println("connection1 = " + connection1.getClass());
+        System.out.println("connection2 = " + connection2);
+        System.out.println("connection2 = " + connection2.getClass());
+    }
+
+    @Test
+    void getDataSourceDriverManagerMax() throws SQLException {
+        final DataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        for (int i = 0; i < 20; i++) {
+            final Connection connection = dataSource.getConnection();
+            System.out.println("connection = " + connection);
+            System.out.println("connection = " + connection.getClass());
+        }
+    }
+}

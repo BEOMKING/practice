97270d8199e5b50e132bd7f73f564b9877d09e55
BEOMKING <qjawlsqjacks@naver.com>
2024-02-26 22:53:36 +0900

    Init Concurrency Stock System

---
+++ Spring/concurrency-stock/src/main/java/org/example/concurrencystock/ConcurrencyStockApplication.java
@@ -0,0 +1,13 @@
+package org.example.concurrencystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyStockApplication.class, args);
+    }
+
+}

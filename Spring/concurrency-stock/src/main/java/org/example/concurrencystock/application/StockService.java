7fb17f6a35b34a7f5c2574a49741a08d6be92621
BEOMKING <qjawlsqjacks@naver.com>
2024-03-03 17:01:41 +0900

    Named Lock Example.

--- Spring/concurrency-stock/src/main/java/org/example/concurrencystock/application/StockService.java
+++ Spring/concurrency-stock/src/main/java/org/example/concurrencystock/application/StockService.java
@@ -3,6 +3,7 @@ package org.example.concurrencystock.application;
 import org.example.concurrencystock.domain.Stock;
 import org.example.concurrencystock.domain.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -13,13 +14,20 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    /**
-     * <p>@Transactional 애노테이션에 의해 synchronized 키워드가 사용되지 않는다.
-     *
-     * @see org.example.concurrencystock.application.TransactionStockService
-     */
-//    @Transactional
-    public synchronized Stock decrease(final Long id, final Long quantity) {
+//    /**
+//     * <p>@Transactional 애노테이션에 의해 synchronized 키워드가 사용되지 않는다.
+//     *
+//     * @see org.example.concurrencystock.application.TransactionStockService
+//     */
+////    @Transactional
+//    public synchronized Stock decrease(final Long id, final Long quantity) {
+//        final Stock stock = stockRepository.findById(id).orElseThrow();
+//        stock.decrease(quantity);
+//        return  stockRepository.saveAndFlush(stock);
+//    }
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public Stock decrease(final Long id, final Long quantity) {
         final Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
         return  stockRepository.saveAndFlush(stock);
---
+++ Spring/concurrency-stock/src/test/java/org/example/concurrencystock/facade/NamedLockStockFacadeTest.java
@@ -0,0 +1,45 @@
+package org.example.concurrencystock.facade;
+
+import org.example.concurrencystock.domain.Stock;
+import org.example.concurrencystock.domain.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class NamedLockStockFacadeTest {
+    @Autowired
+    private NamedLockStockFacade stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @AfterEach
+    void tearDown() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    void concurrentDecrease() throws InterruptedException {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+        final ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+        for (int i = 0; i < 100; i++) {
+            executorService.submit(() -> {
+                Stock stock = stockService.decrease(1L, 1L);
+                System.out.println(stock.getQuantity());
+            });
+        }
+
+        Thread.sleep(5000L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertThat(stock.getQuantity()).isEqualTo(0L);
+    }
+}

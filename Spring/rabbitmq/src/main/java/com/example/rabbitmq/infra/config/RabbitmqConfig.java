2f7f3e6a86c1f258dd755270737de53dd8f8adc6
BEOMKING <qjawlsqjacks@naver.com>
2024-03-28 14:47:32 +0900

    Queue, Exchange,Binding 구성

---
+++ Spring/rabbitmq/src/main/java/com/example/rabbitmq/infra/config/RabbitmqConfig.java
@@ -0,0 +1,47 @@
+package com.example.rabbitmq.infra.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange("topicExchange");
+    }
+
+    @Bean
+    public Queue commandQueue() {
+        return new Queue("command");
+    }
+
+    @Bean
+    public Queue userQueue() {
+        return new Queue("user");
+    }
+
+    @Bean
+    public Queue roomQueue() {
+        return new Queue("room");
+    }
+
+    @Bean
+    public Binding bindingCommandQueue(Queue commandQueue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(commandQueue).to(topicExchange).with("command.#");
+    }
+
+    @Bean
+    public Binding bindingChatUserQueue(Queue userQueue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(userQueue).to(topicExchange).with("chat.*.user.#");
+    }
+
+    @Bean
+    public Binding bindingChatRoomQueue(Queue roomQueue, TopicExchange topicExchange) {
+        return BindingBuilder.bind(roomQueue).to(topicExchange).with("chat.*.room.#");
+    }
+}

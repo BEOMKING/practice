4798b52af3794f55e098e74359380bb0755f6f76
BEOMKING <qjawlsqjacks@naver.com>
2024-05-09 18:33:33 +0900

    Future 예제 구현

---
+++ Spring/spring-async/src/main/java/com/example/springasync/future/FutureExample.java
@@ -0,0 +1,28 @@
+package com.example.springasync.future;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Slf4j
+public class FutureExample {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        final ExecutorService executorService = Executors.newCachedThreadPool();
+
+        final Future<String> future = executorService.submit(() -> {
+            Thread.sleep(2000L);
+            log.info("Async");
+            return "Hello";
+        });
+
+        System.out.println(future.isDone());
+        log.info("Exit");
+        System.out.println(future.isDone());
+        log.info(future.get()); // Blocking 처리 될 때까지 기다림
+
+        executorService.shutdown();
+    }
+}
